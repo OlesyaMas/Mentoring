6fd3fb15e1f5c75d57949bf612e685d55c5116e4
OlesyaMas <maslyuk.lesya@gmail.com>
2018-04-04 23:22:09 +0300

    fixes and added non static methos

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -9,15 +9,15 @@ public class Calculator {
 		
 		double resultMemory = 0;
 		double resultLine = 0;
-		
+		System.out.println("Input number or operator and press Enter.");
+		System.out.println("To calculate result enter '=' ");
+		System.out.println("You can use m+, m-, mr to work with memory.");
 		do {
 			in = new Scanner(System.in);
 			input = in.next();
 			
-			//System.out.println("INPUT = " + input);
-			
 			//TODO add !isExit()
-			if(!Operations.isOperator(input) && !Operations.isMem(input) && !Operations.isOperatorResult(input)){
+			if(!Operations.isOperator(input) && !Operations.isMem(input) && !Operations.isOperatorResult(input) && !Operations.isExit(input)){
 				num = Double.parseDouble(input);
 				resultLine = num;
 			}else{
@@ -36,24 +36,17 @@ public class Calculator {
 				
 				//TODO +,-
 				if(Operations.isMem(input)){
-			        switch (Operations.getMemOperation(input)) {
-			            case MPLUS:
-			            	resultMemory = resultMemory + resultLine;
-			            	break;
-			            case MMINUS:
-			            	resultMemory = resultMemory - resultLine;
-			            	break;
-			        }
-            		System.out.println("MEMORY READ = " + resultMemory);
+			        Processor memory = new Processor();
+			        resultMemory = memory.putToMemo(input, resultLine, resultMemory);
+            		System.out.println("MEMORY VALUE = " + resultMemory);
 				}
+				
 			}
 			
-		} while (!input.equals(Operations.EXIT.getOperation()));
+		} while (!Operations.isExit(input));
 		
 		in.close();
 		
 		System.out.println("Bye");
 	}
-	
-
 }
