ba0d46babb51f3213f2c824f9847393180c35051
o m <sandrm@ukr.net>
2018-04-13 00:12:58 +0300

    Refactored according last requirements

---
+++ Calculator/src/main/java/calculat/Calculator.java
@@ -0,0 +1,46 @@
+package main.java.calculat;
+
+public class Calculator {
+
+	static double num1 = 0;
+    static double num2 = 0;
+    //double resultLine = 0;
+    double resultMemory = 0;
+
+    
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double division(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public static double multi(double num1, double num2) {
+        return num1 * num2;
+    }
+
+
+    public double addToMemo(double num){
+        return resultMemory = resultMemory + num;
+    }
+
+    public double subtractFromMemo(double num){
+        return resultMemory = resultMemory - num;
+    }
+
+    public double readFromMemory(){
+        return resultMemory;
+    }
+
+   /* public static void reset() {
+        num1 = 0;
+        num2 = 0;
+        operation = Operations.UNKNOWN;
+    }*/
+
+}
