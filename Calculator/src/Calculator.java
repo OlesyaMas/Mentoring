8f97565c04b6dbb2e25b0d88faff9a7408e37e80
OlesyaMas <maslyuk.lesya@gmail.com>
2018-04-12 21:51:20 +0300

    Add files via upload
    add class with operations method

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,52 +1,72 @@
-import java.util.Scanner;
+package calculator;
 
 public class Calculator {
-	
-	public static void main(String[] args) {
-		String input = null;
-		Scanner in;
-		double num = 0;
-		
-		double resultMemory = 0;
-		double resultLine = 0;
-		System.out.println("Input number or operator and press Enter.");
-		System.out.println("To calculate result enter '=' ");
-		System.out.println("You can use m+, m-, mr to work with memory.");
-		do {
-			in = new Scanner(System.in);
-			input = in.next();
-			
-			//TODO add !isExit()
-			if(!Operations.isOperator(input) && !Operations.isMem(input) && !Operations.isOperatorResult(input) && !Operations.isExit(input)){
-				num = Double.parseDouble(input);
-				resultLine = num;
-			}else{
-				if(Operations.isOperator(input)){
-					Processor.reset();
-					Processor.operation = Operations.getCodeOperation(input);
-					Processor.num1 = num;					
-					input = in.next();
-					Processor.num2 = Double.parseDouble(input);
-				}
+    static double num1 = 0;
+    static double num2 = 0;
+    double resultLine = 0;
+    double resultMemory = 0;
+    static Operations operation = Operations.UNKNOWN;
 
-				if(Operations.getCodeOperation(input) == Operations.RESULT){
-					resultLine = Processor.calculate();
-					System.out.println("RESULT = " + resultLine);
-				} 
-				
-				//TODO +,-
-				if(Operations.isMem(input)){
-			        Processor memory = new Processor();
-			        resultMemory = memory.putToMemo(input, resultLine, resultMemory);
-            		System.out.println("MEMORY VALUE = " + resultMemory);
-				}
-				
-			}
-			
-		} while (!Operations.isExit(input));
-		
-		in.close();
-		
-		System.out.println("Bye");
-	}
+	
+	/*public static double calculate() {
+        switch (operation) {
+            case PLUS:
+                return num1 + num2;
+            case MINUS:
+                return num1 - num2;
+            case MULTIPLY:
+                return num1 * num2;
+            case DIVISION:
+                return num1 / num2;
+            default:
+                throw new AssertionError("Unknown operations " + operation);
+        }
+    }*/
+
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double division(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public static double multi(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    /*public double putToMemo(String input, double resultLine, double resultMemory) {
+        switch (Operations.getMemOperation(input)) {
+            case MPLUS:
+                resultMemory = resultMemory + resultLine;
+                break;
+            case MMINUS:
+                resultMemory = resultMemory - resultLine;
+                break;
+        }
+        return resultMemory;
+    }*/
+
+    public double addToMemo(){
+        return resultMemory = resultMemory + resultLine;
+    }
+
+    public double subtractFromMemo(){
+        return resultMemory = resultMemory - resultLine;
+    }
+
+    public double readFromMemory(){
+        return resultMemory;
+    }
+
+   /* public static void reset() {
+        num1 = 0;
+        num2 = 0;
+        operation = Operations.UNKNOWN;
+    }*/
+
 }
