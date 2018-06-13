d8e6038963bde6a19c752933f5147c8101acbbc5
OlesyaMas <maslyuk.lesya@gmail.com>
2018-06-14 00:27:33 +0300

    add implemantation classes

---
+++ CurrentDateClientImpl.java
@@ -0,0 +1,29 @@
+package maslyuk.lesya.gmail.com.impl;
+
+import maslyuk.lesya.gmail.com.CurrentDateObserver;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class CurrentDateClientImpl implements CurrentDateObserver {
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMMM yyyy HH:mm:ss", Locale.getDefault());
+    private Date date;
+    private String clientName;
+
+    public CurrentDateClientImpl(String clientName) {
+        this.clientName = clientName;
+    }
+
+    @Override
+    public void update(Date date) {
+        this.date = date;
+        display();
+    }
+
+    public void display() {
+        String result = simpleDateFormat.format(date);
+        System.out.println("The time is now for " + clientName + " " + result);
+    }
+
+}
